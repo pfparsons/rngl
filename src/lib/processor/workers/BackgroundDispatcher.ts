76107307570dcb5be1970f3ecdbafa9fb7e8cc1f
Patrick Parsons <pfparsons>
2021-07-24 20:44:35 -0400

    interim commit

--- src/lib/processor/workers/BackgroundDispatcher.ts
+++ src/lib/processor/workers/BackgroundDispatcher.ts
@@ -1,15 +1,29 @@
 /// <reference lib="webworker" />
 import { EventLog } from "../../EventLog";
+import type { Request, LoadBufferRequest } from "../DispatchMessages";
+import { Instruction } from "../DispatchMessages";
+import Loader from "web-worker:./Loader"
 
 class BackgroundDispatcher {
+
+    
+
     constructor() {
 
     }
 
     handleRequest(event: MessageEvent<Request> ): void {
-        const message = event.data;
+        let request = event.data;
 
-        switch (message.type)
+        switch (request.instruction ){
+            case Instruction.LOAD:
+                request = request as LoadBufferRequest;
+                break;
+        }
+    }
+
+    async dispatchLoadRequest(loadRequest: LoadBufferRequest) {
+
     }
 
 }
